ea74cc1c9c0e72a0264cf1108fc62ec44ab9a0d5
ge90114b <ge90114b@outlook.com>
2024-04-13 21:12:38 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');  
+const copy = require('gulp-copy');   
+const util = require('gulp-util');  
+const fs = require('fs');  
+const path = require('path');  
+  
+// 定义源文件和目标文件夹  
+const srcDir = 'pics/';  
+const destDir = 'dist/';  
+const htmlDest = path.join(destDir, 'files.html');  
+  
+// 复制图片到目标文件夹  
+gulp.task('copy-images', function() {  
+  return gulp.src(srcDir + '**/*')  
+    .pipe(copy(destDir, { prefix: 1 })); // prefix: 1 保持目录结构  
+});  
+  
+// 生成HTML文件  
+gulp.task('generate-html', function(done) {  
+  // 读取图片文件名并存储到数组中  
+  const imageFiles = fs.readdirSync(srcDir)  
+    .filter(file => path.extname(file).match(/\.(png|jpe?g|gif|svg)$/i));  
+  
+  // 将文件名数组转换为JSON字符串  
+  const jsonFilenames = JSON.stringify(imageFiles);  
+    
+    // 将字符字符串嵌入到HTML模板中  
+    const htmlContent = `<!DOCTYPE html>  
+          <html lang="en">  
+          <head>  
+          <meta charset="UTF-8">  
+          <title>Hina</title>   
+          </head>  
+          <body>  
+            <pre>${jsonFilenames}</pre>  
+          </body>  
+          </html>`;  
+    
+    // 将HTML内容写入文件  
+    fs.writeFileSync(htmlDest, htmlContent);  
+    
+    // 通知Gulp任务完成  
+    done();  
+});
+  
+// 默认任务  
+gulp.task('default', gulp.series('copy-images', 'generate-html'));
